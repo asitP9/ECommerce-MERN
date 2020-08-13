916f436b1a348118d49780ce5f979fe1c038c1a5
AsitPanda-at-100487626160 <panda9asit@gmail.com>
2020-08-13 18:20:03 +1000

    API to process the payment through Payment Gateway

--- routes/braintree.js
+++ routes/braintree.js
@@ -2,11 +2,11 @@ const {requireSignInForAuth, isAuth}=require("../controllers/auth");
 const express=require("express");
 const router=express.Router();
 const {userById}=require("../controllers/user");
-const {generateToken}=require("../controllers/braintree");
+const {generateToken, processPayment}=require("../controllers/braintree");
 
 
 router.param('userid', userById);
-router.get("/braintree/getToken/:userid", requireSignInForAuth, isAuth, generateToken)
-
+router.get("/braintree/getToken/:userid", requireSignInForAuth, isAuth, generateToken);
+router.post("/braintree/payment/:userid", requireSignInForAuth, isAuth, processPayment);
 
 module.exports = router;
